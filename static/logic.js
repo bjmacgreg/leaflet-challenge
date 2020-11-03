dc7f12d5efe74c4fe76725f3768e5513269c9e45
Barbara MacGregor <1453-macgregb@users.noreply.umn.bootcampcontent.com>
2020-11-03 15:42:44 -0600

    Final

--- static/logic.js
+++ static/logic.js
@@ -1,50 +1,54 @@
-// Thanks to https://github.com/PetraLee2019/ (suspected former Bootcamp student) for help with plate lines
-// Store our API endpoint inside queryUrl
+
+// Store USGS API endpoint as queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
-//Plate boundaries (get cookie error? But apparently not fatal)
+//Plate boundaries (get cookie error, but apparently not fatal)
 var fraxenPlates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
-console.log(fraxenPlates);
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function (data) {
   // Send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
 
 //Get and style plate boundary lines
+//Thanks to https://github.com/PetraLee2019/ (suspected former Bootcamp student)
 var plateBoundary = new L.LayerGroup();
 d3.json(fraxenPlates, function (boundaries) {
-    L.geoJSON(boundaries.features, {
-        style: function (geoJsonFeature) {
-            return {
-                weight: 2,
-                color: 'magenta'
-            }
-        },
-    }).addTo(plateBoundary);
+  L.geoJSON(boundaries.features, {
+    style: function () {
+      return {
+        weight: 2,
+        color: 'magenta'
+      }
+    },
+  }).addTo(plateBoundary);
 })
 
+// Create a GeoJSON layer containing the earthquake features array on the earthquakeData object
+// Run the onEachFeature function once for each piece of data in the array
 function createFeatures(earthquakeData) {
-  // Define a function to run once for each feature in the features array
   // Make popups showing earthquake time, location, and magnitude
-  function onEachFeature(feature, layer) { 
+  function onEachFeature(feature, layer) {
     let depth = feature.geometry.coordinates[2];
-    let magnitude=  feature.properties.mag;        
+    let magnitude = feature.properties.mag;
     layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>Magnitude: " +magnitude+ "</p> <p>Depth (km): " +depth+"</p>", {maxWidth:"auto"});
-  } 
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>Magnitude: " + magnitude + "</p> <p>Depth (km): " + depth + "</p>", { maxWidth: "auto" });
+  }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  // Thanks to https://codepen.io/dagmara223/pen/LWYNJO (apparently colors aren't colors, they are styles, b/c - who knows)
-  var earthquakes = L.geoJSON(earthquakeData, {
-    style: function(feature) {
-      let depth = feature.geometry.coordinates[2]; 
-      if (depth >= 90) {
+  // Assign marker colors by earthquake depth (thanks to https://codepen.io/dagmara223/pen/LWYNJO)
+  let earthquakes = L.geoJSON(earthquakeData, {
+    style: function (feature) {
+      let depth = feature.geometry.coordinates[2];
+      if (depth >= 150) {
         return {
-          fill: "red"
-        }; 
+          color: "brown"
+        };
+      }
+      else if (depth >= 90) {
+        return {
+          color: "red"
+        };
       }
       else if (depth >= 70) {
         return {
@@ -60,7 +64,7 @@ function createFeatures(earthquakeData) {
         };
       } else if (depth >= 10) {
         return {
-          color: "yellowgreen"
+          color: "greenyellow"
         };
       } else {
         return {
@@ -69,24 +73,24 @@ function createFeatures(earthquakeData) {
       }
     },
 
+  //Place circular markers at each feature, with radius defined by earthquake magnitude
     onEachFeature: onEachFeature,
-    pointToLayer: function(feature, latlng) {
+    pointToLayer: function (feature, latlng) {
       return new L.CircleMarker(latlng, {
-        radius: (2**(feature.properties.mag))/5, 
+        radius: (2 ** (feature.properties.mag)) / 5,
         fillOpacity: 0.85,
-
       });
-  },
-
+    },
   });
 
   //Send earthquakes layer to the createMap function
   createMap(earthquakes);
 };
 
+//Assemble the map
 function createMap(earthquakes) {
   //Define streetmap and satellite layers
-    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     id: "mapbox/satellite-streets-v9",
     accessToken: API_KEY,
@@ -116,18 +120,17 @@ function createMap(earthquakes) {
     "Earthquakes": earthquakes
   };
 
-  //Create map, with streetmap and earthquakes layers to be displayed on load
+  //Create map, with satellitemap and earthquakes layers to be displayed on load
   var myMap = L.map("map", {
     center: [
-      0, -0
+      0, 0
     ],
-    zoom: 3,
-    layers: [streetmap, earthquakes]
+    zoom: 2,
+    layers: [satellitemap, earthquakes],
   });
 
   //Layer control for toggling base and overlay layers (glad this is written for us!)
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
-
 }
